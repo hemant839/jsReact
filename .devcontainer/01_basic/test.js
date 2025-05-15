c8bce16d19e8e929d9e50144af01ecc7fc7aaf8f
hemant839 <hemantkumarsahu273@gmail.com>
2025-05-15 05:39:10 +0000

    Math Done

--- .devcontainer/01_basic/test.js
+++ .devcontainer/01_basic/test.js
@@ -1,1 +1,33 @@
-console.log("Hemant");
+//console.log("Hemant");
+
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const bal = 123.8956
+// console.log(bal.toPrecision(4));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+ console.log(Math.random());
+ console.log((Math.random()*10) +1);
+ console.log(Math.floor(Math.random()*10) +1);
+
+ const min = 10
+ const max = 20
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
+ 
+ 
+ 
+
+
+
+
+
+
