4be6a2c7291e3d3b7fca073330bae46d25a9852f
hemant839 <hemantkumarsahu273@gmail.com>
2025-05-20 07:14:57 +0000

    All Push

--- 01_Basic/00_array/01_array.js
+++ 01_Basic/00_array/01_array.js
@@ -31,12 +31,23 @@ let newDate = new Date();
 console.log(newDate.getDate());
 console.log(newDate.getDay());
 
+const myArr1 = new Array("hement","Ila")  
+//console.log(myArr1[1])
 
- 
- 
+const myArr = [1,2,3,4,5]
+// myArr.push(6,7);
 
+const newArr = myArr.join()
+// console.log(newArr);
 
+const myArr2 = myArr.slice(1, 3)
+const myArr3 = myArr.splice(1, 3)
+// console.log(myArr);
+// console.log(myArr2);
+// console.log(myArr3);
 
+const myHero = ["ila","putu","babu"]
+const myheroine = ["babu1","binu","gota"]
 
-
-
+const all_hero = [...myHero, ...myheroine]
+console.log(all_hero);
--- 01_Basic/00_array/02_object.js
+++ 01_Basic/00_array/02_object.js
@@ -62,10 +62,3 @@ const course = {
 } 
 const {courseInstructor: instructor} = course
 console.log(instructor);
-
-
-
-
-
-
-
